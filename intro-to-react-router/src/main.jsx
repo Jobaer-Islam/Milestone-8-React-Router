6ce1577aef96ef3fa1707065df5ea6a4ff699d39
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-07-06 09:40:56 +0600

    created dynamic path and rendered dynamic Route

--- intro-to-react-router/src/main.jsx
+++ intro-to-react-router/src/main.jsx
@@ -12,6 +12,7 @@ import Mobiles from './components/Mobiles/Mobiles.jsx';
 import Laptops from './components/Laptops/Laptops.jsx';
 import Users from './components/Users/Users.jsx';
 import Users2 from './components/Users2/Users2.jsx';
+import UserDetails from './components/UserDetails/UserDetails.jsx';
 
 const userPromise = fetch('https://jsonplaceholder.typicode.com/users').then(res=> res.json());
 
@@ -33,6 +34,10 @@ const router = createBrowserRouter([
         element: <Suspense fallback={<span>Loading..</span>}>
           <Users2 userPromise={userPromise}></Users2>
         </Suspense>
+      },
+      {
+        path: 'users/:userId',
+        Component: UserDetails
       }
 
     ]
