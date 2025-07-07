d8192d59e7413dd4c1143c04895cc5553fedd4bc
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-07-07 19:51:23 +0600

    accessed form data using onSubmit and e target

---
+++ explore-react-form/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Explore React Form</h1>
+      <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
