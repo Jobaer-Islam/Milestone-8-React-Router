012e2a3d22aaf4f97ff0161ae70ad7e3ff673913
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-07-08 22:09:18 +0600

    applied context API

--- explore-react-form/src/components/FamilyTree/FamilyTree.jsx
+++ explore-react-form/src/components/FamilyTree/FamilyTree.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import GrandPa from './GrandPa';
 import './FamilyTree.css';
+
+export const AssetContext = createContext('');
+export const MoneyContext = createContext(0);
 const FamilyTree = () => {
+    const [money, setMoney] = useState(0)
+
+    const asset = 'diamond';
+    const newAsset = 'gold';
     return (
         <div className='family-tree'>
             <h2>Family Tree</h2>
-            <GrandPa></GrandPa>
+            <h3>Total family money: {money}</h3>
+            <MoneyContext value={[money, setMoney]}>
+                <AssetContext.Provider value={newAsset}>
+                    <GrandPa asset={asset}></GrandPa>
+                </AssetContext.Provider>
+            </MoneyContext>  
         </div>
     );
 };
--- explore-react-form/src/components/FamilyTree/Special.jsx
+++ explore-react-form/src/components/FamilyTree/Special.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AssetContext } from './FamilyTree';
 
 const Special = () => {
+    const newAsset = useContext(AssetContext);
+    console.log('new asset', newAsset);
     return (
         <div>
             <h3>Special</h3>
+            
+            <p>New Asset: {newAsset}</p>
+            
         </div>
     );
 };
